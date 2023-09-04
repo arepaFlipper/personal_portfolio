20e546aa9fdb3110f607047767e58d2485e3b07f
Cristian F. Tovar <cristian00tovar@gmail.com>
2023-09-04 17:31:22 -0500

    🤟 (35:14) Develop the Navbar scene

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import { useState } from 'react';
 import useMediaQuery from '@/hooks/useMediaQuery';
+import { Navbar } from './scenes';
 import './App.css'
 
 const App = () => {
   const [selectedPage, setSelectedPage] = useState('home');
-  const isAboveMediumScreens = useMediaQuery
+  const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
   return (
-    <>
-      App
-    </>
+    <div className="app bg-deep-blue">
+      <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+    </div>
   )
 }
 
