e1d086f46d95366c7f78d43bc59ef42f52a19e76
Cristian F. Tovar <cristian00tovar@gmail.com>
2023-09-04 17:45:36 -0500

    ㊗️ (42:55) Display the links options in the Navbar for mobile

--- src/scenes/Navbar.tsx
+++ src/scenes/Navbar.tsx
@@ -47,6 +47,24 @@ const Navbar = ({ selectedPage, setSelectedPage }: TNavbar) => {
             <img alt="menu-icon" src="/src/assets/menu-icon.svg" />
           </button>
         )}
+
+        {!isAboveSmallScreens && isMenuToggled && (
+          <div className="fixed right-0 bottom-0 h-full bg-blue w-[300px]">
+            <div className="flex justify-end p-12">
+              <button onClick={() => setIsMenuToggled(!isMenuToggled)}>
+                <img alt="close-icon" src="/src/assets/close-icon.svg" />
+              </button>
+            </div>
+
+            <div className="flex flex-col gap-10 ml-[33%] text-2xl text-deep-blue">
+              <Link page="Home" selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+              <Link page="Skills" selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+              <Link page="Projects" selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+              {/* <Link page="Testimonials" selectedPage={selectedPage} setSelectedPage={setSelectedPage} /> */}
+              <Link page="Contact" selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+            </div>
+          </div>
+        )}
       </div>
     </nav>
   )
