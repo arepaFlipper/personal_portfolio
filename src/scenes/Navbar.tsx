fa1dc6a7a31b370bbb533ad1d5b839823770b2ab
Cristian F. Tovar <cristian00tovar@gmail.com>
2023-09-04 17:36:48 -0500

    💌 (38:51) Display burger menu at mobile screens

--- src/scenes/Navbar.tsx
+++ src/scenes/Navbar.tsx
@@ -37,8 +37,16 @@ const Navbar = ({ selectedPage, setSelectedPage }: TNavbar) => {
         {isAboveSmallScreens ? (
           <div className="flex justify-between gap-16 font-opensans text-sm font-semibold">
             <Link page="Home" selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+            <Link page="Skills" selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+            <Link page="Projects" selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+            {/* <Link page="Testimonials" selectedPage={selectedPage} setSelectedPage={setSelectedPage} /> */}
+            <Link page="Contact" selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
           </div>
-        ) : (<div></div>)}
+        ) : (
+          <button className="rounded-full bg-red p-2" onClick={() => setIsMenuToggled(!isMenuToggled)}>
+            <img alt="menu-icon" src="/src/assets/menu-icon.svg" />
+          </button>
+        )}
       </div>
     </nav>
   )
