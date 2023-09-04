d1da59862db5c14cb106a96cc744bca7f57cf69d
Cristian F. Tovar <cristian00tovar@gmail.com>
2023-09-04 18:16:54 -0500

    🚡 (53:39) Get the DOT

--- src/scenes/DotGroup.tsx
+++ src/scenes/DotGroup.tsx
@@ -1,11 +1,21 @@
-type Props = {
+import AnchorLink from "react-anchor-link-smooth-scroll"
+type TDotGroup = {
   selectedPage: string;
   setSelectedPage: (page: string) => void;
 }
 
-const DotGroup = (props: Props) => {
+const DotGroup = ({ selectedPage, setSelectedPage }: TDotGroup) => {
+  const selectedStyles = `relative bg-yellow before:absolute before:w-6
+          before:h-6 before:rounded-full before:border-2 before:border-yellow
+          before:left-[-50%] before:top-[-50%]`;
   return (
-    <div>DotGroup</div>
+    <div className="flex flex-col gap-6 fixed top-[60%] right-7">
+      <AnchorLink
+        className={`${(selectedPage === 'home') ? selectedStyles : "bg-dark-grey"} w-3 h-3 rounded-full`}
+        href="#home"
+        onClick={() => setSelectedPage('home')}
+      />
+    </div>
   )
 }
 
