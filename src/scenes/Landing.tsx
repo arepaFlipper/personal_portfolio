90eeb0c7c3fddf0149b23cf0d2a36b81d70de88a
Cristian F. Tovar <cristian00tovar@gmail.com>
2023-09-06 16:47:56 -0500

    🧸 (1:05:48) Display profile-image

--- src/scenes/Landing.tsx
+++ src/scenes/Landing.tsx
@@ -1,8 +1,29 @@
-type Props = {}
+import useMediaQuery from '@/hooks/useMediaQuery';
+import { motion } from 'framer-motion';
+import AnchorLink from 'react-anchor-link-smooth-scroll';
 
-const Landing = (props: Props) => {
+type TLanding = {
+  setSelectedPage: (value: string) => void;
+}
+
+const Landing = ({ setSelectedPage }: TLanding) => {
+  const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
   return (
-    <div>Landing</div>
+    <section id="home" className="md:flex md:justify-between md:items-center md:h-full gap-16 py-10">
+      <div className="md:order-2 flex justify-center basis-3/5 z-10 mt-16 md:mt-32">
+        {isAboveMediumScreens ? (
+          <div className="relative z-0 ml-20 before:absolute 
+          before:-top-20 before:-left-20 before:rounded-t-[400px]
+          before:w-full before:max-w-[400px] before:h-full 
+          before:border-2 before:border-blue before:z-[-1]">
+            <img className="hover:filter hover:saturate-200 transition duration-500 z-10 w-full max-w-[400px] md:max-w-[600px]" src="/src/assets/profile-image.png" alt="profile" />
+          </div>
+        ) : (
+          <div className="relative w-[130px] md:w-[170px]">
+          </div>
+        )}
+      </div>
+    </section>
   )
 }
 
