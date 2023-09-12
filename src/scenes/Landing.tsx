187f056fa0dc19c9122478e958825b6b0ff79d29
Cristian F. Tovar <cristian00tovar@gmail.com>
2023-09-12 15:53:53 -0500

    🏓 use a 3D image

--- src/scenes/Landing.tsx
+++ src/scenes/Landing.tsx
@@ -2,6 +2,7 @@ import useMediaQuery from '@/hooks/useMediaQuery';
 import { SocialMediaIcons } from '@/components';
 import { motion } from 'framer-motion';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
+import ComputersCanvas from '@/scenes/Computers';
 
 type TLanding = {
   setSelectedPage: (value: string) => void;
@@ -39,16 +40,7 @@ const Image_section = () => {
   const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
   return (
     <div className="md:order-2 flex justify-center basis-3/5 z-10 mt-16 md:mt-32">
-      {isAboveMediumScreens ? (
-        <div className="relative z-0 ml-20 before:absolute 
-          before:-top-20 before:-left-20 before:rounded-t-[400px]
-          before:w-full before:max-w-[400px] before:h-full 
-          before:border-2 before:border-blue before:z-[-1]">
-          <img className="hover:filter hover:saturate-200 transition duration-500 z-10 w-full max-w-[400px] md:max-w-[600px]" src="/src/assets/profile-image.png" alt="profile" />
-        </div>
-      ) : (
-        <img className="hover:filter hover:saturate-200 transition duration-500 z-10 w-full max-w-[400px] md:max-w-[600px]" src="/src/assets/profile-image.png" alt="profile" />
-      )}
+      <ComputersCanvas />
     </div>
 
   )
