6c3b4be0b7b8c7c4082065d61d3f4be1bed84efb
Cristian F. Tovar <cristian00tovar@gmail.com>
2024-01-13 12:40:41 -0500

    🫗 add the SocialMedia data

--- src/components/SocialMediaIcons.tsx
+++ src/components/SocialMediaIcons.tsx
@@ -1,20 +1,24 @@
-type Props = {}
+type TIcon = {
+  name: string;
+  url: string;
+  file_name: string;
+}
 
 const SocialMediaIcons = () => {
-  const icons = [
-    "linkedin",
-    "instagram",
-    "twitter",
-    "facebook",
-    // "github",
-    // "leetcode",
+  const icons: TIcon[] = [
+    {"name": "github", "url": "https://www.github.com/arepaflipper/", "file_name": "github-mark-white.png"},
+    {"name": "leetcode", "url": "https://www.leetcode.com/cristian00tovar/", "file_name": "leetcode.svg"},
+    {"name": "linkedin", "url": "https://www.linkedin.com/in/cristian00tovar/", "file_name": "linkedin.png"},
+    {"name": "instagram", "url": "https://www.instagram.com/cristiam182/", "file_name": "instagram.png"},
+    {"name": "facebook", "url": "https://www.facebook.com/cristianfam/", "file_name": "facebook.png"},
+    {"name": "twitter", "url": "https://www.twitter.com/cristiam182/", "file_name": "twitter.png"},
   ]
   return (
     <div className="flex justify-center md:justify-start my-10 gap-7">
       {icons.map((icon) => {
         return (
-          <a className="hover:opacity-50 transition duration-500" href={`https://www.${icon}.com/`} target="_blank" rel="noreferrer" key={icon}>
-            <img alt={icon} src={`/${icon}.png`} />
+          <a className="hover:opacity-50 transition duration-500 w-10 h-10" href={icon.url} target="_blank" rel="noreferrer" key={icon.name}>
+            <img className="w-full h-full object-contain" alt={icon.name} src={icon.file_name} />
           </a>
         )
       }
