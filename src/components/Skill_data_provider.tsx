55400af16bce339209311c58196adbdf6baf0f4d
Cristian F. Tovar <cristian00tovar@gmail.com>
2024-08-01 16:18:35 -0500

    🛣️ add tooltip

--- src/components/Skill_data_provider.tsx
+++ src/components/Skill_data_provider.tsx
@@ -16,13 +16,15 @@ interface Props {
 
 function Skill_data_provider({ data, index, type }: Props) {
   return (
-    <motion.div variants={fadeIn("right", "spring", index * 0.5, 0.75)}>
+    <motion.div
+      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+      className="relative group"
+    >
       <div
-        className={` ${type === "Backend_skill"
+        className={`${type === "Backend_skill"
           ? "w-[35px] 400:w-[45px] 800:w-[50px]"
           : "w-[40px] 400:w-[50px] 800:w-[55px]"
-          } cursor-pointer z-0   1300:w-[${data.width
-          }] h-auto rounded-[5px] z-20     relative  mx-[10px] my-[5px] `}
+          } cursor-pointer z-0 1300:w-[${data.width}px] h-auto rounded-[5px] z-20 relative mx-[10px] my-[5px]`}
       >
         <img
           alt={data.skill_name}
@@ -31,6 +33,9 @@ function Skill_data_provider({ data, index, type }: Props) {
           src={data.Image}
           className=""
         />
+      </div>
+      <div className="absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 px-2 py-1 bg-gray-800 text-white text-xl rounded opacity-0 group-hover:opacity-100 transition-opacity duration-300 whitespace-nowrap pointer-events-none z-30">
+        {data.skill_name}
       </div>
     </motion.div>
   );
