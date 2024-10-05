e837378df540696809906ac379ab270312ec8953
Cristian F. Tovar <cristian00tovar@gmail.com>
2024-10-05 09:07:12 -0500

    🍮 fix the Home type Props

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,12 @@
 import { useState, useEffect } from 'react';
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import useMediaQuery from '@/hooks/useMediaQuery';
 import { LineGradient } from '@/components';
 import { Navbar, Projects, Contact, Footer, DotGroup, Landing, MySkills, Technologies } from './scenes';
 import CV from './cv/CV';
 import "./index.css";
 
-const Home = ({ selectedPage, setSelectedPage, isAboveMediumScreens }) => (
-  <>
-    <Navbar isTopOfPage={isAboveMediumScreens} selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
-    <div className="w-5/6 mx-auto md:h-full">
-      {isAboveMediumScreens && (
-        <DotGroup selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
-      )}
-      <Landing setSelectedPage={setSelectedPage} />
-    </div>
-
-    <LineGradient width="w-full" />
-    <div className="w-5/6 mx-auto">
-      <MySkills />
-    </div>
-
-    <LineGradient width="w-full" />
-    <div className="w-5/6 mx-auto">
-      <Technologies />
-    </div>
-
-    <LineGradient width="w-full" />
-    <div className="w-5/6 mx-auto">
-      <Projects />
-    </div>
-
-    <LineGradient width="w-full" />
-    <div className="w-5/6 mx-auto">
-      <Contact />
-    </div>
-
-    <Footer />
-  </>
-);
-
-const App = () => {
+const Home = () => {
   const [selectedPage, setSelectedPage] = useState('home');
   const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
   const [isTopOfPage, setIsTopOfPage] = useState(true);
@@ -55,9 +21,45 @@ const App = () => {
   }, [])
 
   return (
+    <>
+      <Navbar isTopOfPage={isAboveMediumScreens} selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+      <div className="w-5/6 mx-auto md:h-full">
+        {isAboveMediumScreens && (
+          <DotGroup selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+        )}
+        <Landing setSelectedPage={setSelectedPage} />
+      </div>
+
+      <LineGradient width="w-full" />
+      <div className="w-5/6 mx-auto">
+        <MySkills />
+      </div>
+
+      <LineGradient width="w-full" />
+      <div className="w-5/6 mx-auto">
+        <Technologies />
+      </div>
+
+      <LineGradient width="w-full" />
+      <div className="w-5/6 mx-auto">
+        <Projects />
+      </div>
+
+      <LineGradient width="w-full" />
+      <div className="w-5/6 mx-auto">
+        <Contact />
+      </div>
+
+      <Footer />
+    </>
+  )
+};
+
+const App = () => {
+  return (
     <div className="h-full bg-deep-blue">
       <Routes>
-        <Route path="/" element={<Home selectedPage={selectedPage} setSelectedPage={setSelectedPage} isAboveMediumScreens={isAboveMediumScreens} />} />
+        <Route path="/" element={<Home />} />
         <Route path="/cv" element={<CV />} />
       </Routes>
     </div>
